326962b36d85a1d4f10e0de6a1f6273f9b94b77d
superzish <zjulbertg@gmail.com>
2016-09-13 17:40:47 +0800

    first

---
+++ www/js/app.js
@@ -0,0 +1,198 @@
+// Ionic iComPAsS App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'iComPAsS' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'iComPAsS.controllers' is found in controllers.js
+// 'iComPAsS.sevices' is found in services.js
+angular.module('iComPAsS', ['ionic', 'iComPAsS.controllers', 'iComPAsS.services', 'pascalprecht.translate'])
+
+.run(function($ionicPlatform, $ionicPopup) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+
+    // Checks if there is Internet
+    if(window.Connection) {
+      if(navigator.connection.type == Connection.NONE) {
+        $ionicPopup.confirm({
+          title: "Internet Disconnected",
+          content: "The internet is disconnected on your device."
+        })
+        .then(function(result) {
+          if(!result) {
+              ionic.Platform.exitApp();
+          }
+        });
+      }
+    }
+  });
+})
+
+.config(function($stateProvider, $urlRouterProvider, $translateProvider, USER_ROLES) {
+  // Routes
+  $stateProvider
+
+  .state('login', {
+    url: '/login',
+    templateUrl: 'templates/login.html',
+    controller: 'LoginCtrl',
+    onEnter: function($state, AuthService){
+      if(AuthService.isAuthenticated()){
+        $state.go('menu.profile');
+      }
+    }
+  })
+
+  .state('menu', {
+    url: '/menu',
+    abstract: true,
+    templateUrl: 'templates/menu.html',
+    controller: 'MenuCtrl'
+  })
+
+  .state('menu.profile', {
+    url: '/profile',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/profile.html',
+        controller: 'ProfileCtrl'
+      }
+    }
+  })
+
+  .state('menu.prescriptions', {
+    url: '/prescriptions',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/prescriptions.html',
+        controller: 'PrescriptionsCtrl'
+      }
+    }
+  })
+
+  .state('menu.take-esas', {
+    url: '/take-esas',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/take-esas.html'
+      }
+    }
+  })
+
+  .state('menu.esas-results', {
+    url: '/esas-results',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/esas-results.html'
+      }
+    }
+  })
+
+  .state('menu.list-of-doctors', {
+    url: '/list-of-doctors',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/list-of-doctors.html',
+        controller: 'ListOfDoctorCtrl'
+      }
+    }
+  })
+
+  .state('menu.list-of-patients', {
+    url: '/list-of-patients',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/list-of-patients.html'
+      }
+    }
+  })
+
+  .state('menu.doctor-profile', {
+    url: '/doctor-profile/:doctorId',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/doctor-profile.html',
+        controller: 'DoctorProfileCtrl'
+      }
+    }
+  })
+
+  .state('menu.instructions', {
+    url: '/instructions',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/instructions.html'
+      }
+    }
+  })
+
+  .state('menu.inbox', {
+    url: '/inbox',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/inbox.html'
+      }
+    }
+  })
+
+  .state('menu.message', {
+    url: '/message',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/message.html'
+      }
+    }
+  })
+
+  .state('menu.change-password', {
+    url: '/change-password',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/change-password.html'
+      }
+    }
+  })
+
+  .state('menu.help', {
+    url: '/help',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/help.html'
+      }
+    }
+  });
+
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('login');
+
+  // Translate
+  $translateProvider
+  .useStaticFilesLoader({
+    prefix: 'js/locales/',
+    suffix: '.json'
+  })
+  .registerAvailableLanguageKeys(['en', 'fil'], {
+    'en' : 'en', 'en_GB': 'en', 'en_US': 'en',
+    'fil' : 'fil'
+  })
+  .preferredLanguage('en')
+  .fallbackLanguage('en')
+  .determinePreferredLanguage()
+  .useSanitizeValueStrategy('escapeParameters');
+})
+
+.constant('USER_ROLES', {
+  admin: 'admin_role',
+  patient: 'patient_role',
+  doctor: 'doctor_role'
+});
---
+++ www/js/controllers.js
@@ -0,0 +1,357 @@
+angular.module('iComPAsS.controllers', [])
+
+.controller('AppCtrl', function($scope, $state, $ionicHistory, AuthService) {
+
+  $scope.doLogout = function() {
+    console.log('Doing logout');
+
+    // Destroy saved credentials
+    AuthService.logout();
+
+    // Change state into login
+    $state.go('login').then(function(){
+      $scope.clearBackView();
+    });
+  };
+
+  $scope.clearBackView = function() {
+    $ionicHistory.clearCache();
+    $ionicHistory.clearHistory();
+  };
+})
+
+.controller('LoginCtrl', function($scope, $state, $ionicPopup, AuthService, USER_ROLES) {
+
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+
+  // Form data for the login modal
+  $scope.loginData = {};
+
+  // Perform the login action when the user submits the login form
+  $scope.doLogin = function() {
+    console.log('Doing login', $scope.loginData);
+
+    AuthService.login($scope.loginData.username, $scope.loginData.password).then(function(authenticated) {
+      // Prevent admin account from logging in
+      if(AuthService.role() === USER_ROLES.admin){
+        //destroy user credential becuase login was not really made
+        AuthService.logout();
+        var alertPopup = $ionicPopup.alert({
+          title: 'Sorry!',
+          template: 'Android verion of iComPAsS is not available to this type of user.',
+          cssClass: 'alert-popup'
+        });
+      }else{
+        //redirect to profile
+        $state.go('menu.profile', {}, {reload: true}).then(function(){
+          $scope.clearBackView();
+        });
+      }
+
+      // Clear loginData
+      $scope.loginData = {};
+    }, function(err) {
+      var alertPopup = $ionicPopup.alert({
+        title: 'Login failed!',
+        template: 'Please check your credentials!',
+        cssClass: 'alert-popup'
+      });
+
+      // Clear loginData
+      $scope.loginData = {};
+    });
+  };
+})
+
+.controller('MenuCtrl', function($scope, $state, AuthService, USER_ROLES) {
+
+  // list of menu
+  if(AuthService.role() === USER_ROLES.patient){
+    $scope.menuList = [
+      {
+        'icon': 'ion-ios-person',
+        'text': 'Profile',
+        'url': '#/menu/profile'
+      },
+      {
+        'icon': 'ion-document',
+        'text': 'Prescriptions',
+        'url': '#/menu/prescriptions'
+      },
+      {
+        'icon': 'ion-ios-body',
+        'text': 'Take ESAS',
+        'url': '#/menu/take-esas'
+      },
+      {
+        'icon': 'ion-ios-people',
+        'text': 'List of Doctors',
+        'url': '#/menu/list-of-doctors'
+      },
+      {
+        'icon': 'ion-ios-book',
+        'text': 'Instructions',
+        'url': '#/menu/instructions'
+      },
+      {
+        'icon': 'ion-email',
+        'text': 'Inbox',
+        'url': '#/menu/inbox'
+      },
+      {
+        'icon': 'ion-key',
+        'text': 'Change Password',
+        'url': '#/menu/change-password'
+      },
+      {
+        'icon': 'ion-ios-help',
+        'text': 'Help',
+        'url': '#/menu/help'
+      },
+    ];
+  }else if(AuthService.role() === USER_ROLES.doctor){
+    $scope.menuList = [
+      {
+        'icon': 'ion-ios-person',
+        'text': 'Profile',
+        'url': '#/menu/profile'
+      },
+      {
+        'icon': 'ion-ios-people',
+        'text': 'List of Patients',
+        'url': '#/menu/list-of-patients'
+      },
+      {
+        'icon': 'ion-ios-body',
+        'text': 'ESAS Results',
+        'url': '#/menu/esas-results'
+      },
+      {
+        'icon': 'ion-email',
+        'text': 'Inbox',
+        'url': '#/menu/inbox'
+      },
+      {
+        'icon': 'ion-email',
+        'text': 'Message',
+        'url': '#/menu/message'
+      },
+      {
+        'icon': 'ion-key',
+        'text': 'Change Password',
+        'url': '#/menu/change-password'
+      },
+      {
+        'icon': 'ion-ios-help',
+        'text': 'Help',
+        'url': '#/menu/help'
+      },
+    ];
+  }
+})
+
+.controller('ProfileCtrl', function($scope, AuthService, USER_ROLES, APIService){
+
+  $scope.user_profile = {};
+
+  var setBasicInfo = function(data){
+    $scope.user_profile.image = data.image;
+
+    $scope.user_profile.username = data.username;
+
+    $scope.user_profile.basic_info = [
+      {
+        'icon': 'ion-person',
+        'data': data.fname + ' ' + data.mname + ' ' + data.lname
+      },
+      {
+        'icon': 'ion-ios-telephone',
+        'data': '(+63) ' + data.contactnumber
+      },
+      {
+        'icon': 'ion-email',
+        'data': data.email
+      },
+      {
+        'icon': 'ion-calendar',
+        'data': data.bday
+      },
+      {
+        'icon': 'ion-person',
+        'data': data.age
+      },
+      {
+        'icon': 'ion-transgender',
+        'data': data.gender === 'm'? 'Male' : 'Female'
+      }
+    ];
+  };
+
+  if(AuthService.role() === USER_ROLES.patient){
+    APIService.get_patient_profile().then(function(data) {
+
+      setBasicInfo(data);
+
+      // Parse JSON form data.weights
+      var weights = [];
+      for (var i = 0; i < data.weights.length; i++) {
+        weights.push(JSON.parse(data.weights[i]));
+      }
+
+      $scope.user_profile.more = [
+        {
+          'label': 'Diagnosis',
+          'data': [data.diagnosis]
+        },
+        {
+          'label': 'Allergies',
+          'data': data.allergies.split(', ')
+        },
+        {
+          'label': 'Initial Height',
+          'data': [data.height + ' cm']
+        },
+        {
+          'label': 'Weight',
+          'data': weights
+        }
+      ];
+    });
+  }else if(AuthService.role() === USER_ROLES.doctor){
+    APIService.get_doctor_profile().then(function(data) {
+
+      setBasicInfo(data);
+
+      $scope.user_profile.more = [
+        {
+          'label': 'Specialty',
+          'data': [data.specialty]
+        },
+        {
+          'label': 'Availability',
+          'data': [data.available]
+        }
+      ];
+    });
+  }
+
+  // Tab functionalities
+  $scope.classes = {};
+  $scope.classes.basic_info_tab = ['active'];
+  $scope.classes.more_tab = [];
+  $scope.classes.basic_info = [];
+  $scope.classes.more = ['hidden'];
+  $scope.showBasicInfo = function () {
+    if($scope.classes.basic_info_tab.length < 1){
+      $scope.classes.basic_info_tab.push('active');
+    }
+    $scope.classes.basic_info.pop('hidden');
+    $scope.classes.more_tab.pop('active');
+    if($scope.classes.more.length < 1){
+      $scope.classes.more.push('hidden');
+    }
+  };
+  $scope.showMore = function () {
+    $scope.classes.basic_info_tab.pop('active');
+    if($scope.classes.basic_info.length < 1){
+      $scope.classes.basic_info.push('hidden');
+    }
+    if($scope.classes.more_tab.length < 1){
+      $scope.classes.more_tab.push('active');
+    }
+    $scope.classes.more.pop('hidden');
+  };
+})
+
+.controller('PrescriptionsCtrl', function($scope, APIService){
+  APIService.get_patient_prescriptions().then(function(data) {
+    $scope.prescriptions = data;
+  });
+})
+
+.controller('ListOfDoctorCtrl', function($scope, APIService){
+  APIService.get_assigned_doctors().then(function(data) {
+    $scope.assign_doctors = data;
+  });
+})
+
+.controller('DoctorProfileCtrl', function($scope, $stateParams, APIService){
+  $scope.doctor_profile = {};
+
+  APIService.get_doctor_info($stateParams.doctorId).then(function(data) {
+
+    $scope.doctor_profile.image = data.image;
+
+    $scope.doctor_profile.username = data.username;
+
+    $scope.doctor_profile.basic_info = [
+      {
+        'icon': 'ion-person',
+        'data': data.fname + ' ' + data.mname + ' ' + data.lname
+      },
+      {
+        'icon': 'ion-ios-telephone',
+        'data': '(+63) ' + data.contactnumber
+      },
+      {
+        'icon': 'ion-email',
+        'data': data.email
+      },
+      {
+        'icon': 'ion-calendar',
+        'data': data.bday
+      },
+      {
+        'icon': 'ion-person',
+        'data': data.age
+      },
+      {
+        'icon': 'ion-transgender',
+        'data': data.gender === 'm'? 'Male' : 'Female'
+      }
+    ];
+
+    $scope.doctor_profile.more = [
+      {
+        'label': 'Specialty',
+        'data': [data.specialty]
+      },
+      {
+        'label': 'Availability',
+        'data': [data.available]
+      }
+    ];
+  });
+
+  // Tab functionalities
+  $scope.classes = {};
+  $scope.classes.basic_info_tab = ['active'];
+  $scope.classes.more_tab = [];
+  $scope.classes.basic_info = [];
+  $scope.classes.more = ['hidden'];
+  $scope.showBasicInfo = function () {
+    if($scope.classes.basic_info_tab.length < 1){
+      $scope.classes.basic_info_tab.push('active');
+    }
+    $scope.classes.basic_info.pop('hidden');
+    $scope.classes.more_tab.pop('active');
+    if($scope.classes.more.length < 1){
+      $scope.classes.more.push('hidden');
+    }
+  };
+  $scope.showMore = function () {
+    $scope.classes.basic_info_tab.pop('active');
+    if($scope.classes.basic_info.length < 1){
+      $scope.classes.basic_info.push('hidden');
+    }
+    if($scope.classes.more_tab.length < 1){
+      $scope.classes.more_tab.push('active');
+    }
+    $scope.classes.more.pop('hidden');
+  };
+});
