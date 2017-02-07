23f1843a7cea56052e4e7349bb7721f91b528a6f
jporciuncula <jacob.porciuncula@altpaynet.com>
2017-02-07 10:02:11 +0800

    something about back button

--- www/js/controllers/app.controller.js
+++ www/js/controllers/app.controller.js
@@ -110,9 +110,7 @@ angular.module('iComPAsS.controllers')
       navigator.app.exitApp();
     }
 
-    if ($scope.tab > 1) {
-      $scope.setTab(1);
-    }else if ($ionicHistory.viewHistory().histories.ion2.cursor === 0 && $state.current.name !== "menu.profile") {
+    if ($ionicHistory.viewHistory().histories.ion2.cursor === 0 && $state.current.name !== "menu.profile") {
       $ionicHistory.nextViewOptions({
         disableBack: true
       });
