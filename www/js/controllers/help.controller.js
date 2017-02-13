d975828a375f087cee87554463fbc1679653ef9f
superzish <zjulbertg@gmail.com>
2017-02-13 23:34:54 +0800

    help more awesome

--- www/js/controllers/help.controller.js
+++ www/js/controllers/help.controller.js
@@ -1,21 +1,11 @@
 angular.module('iComPAsS.controllers')
 
 .controller('HelpCtrl', function($scope){
-  $scope.groups = [];
-  for (var i=0; i<10; i++) {
-    $scope.groups[i] = {
-      name: i,
-      items: []
-    };
-    for (var j=0; j<3; j++) {
-      $scope.groups[i].items.push(i + '-' + j);
-    }
-  }
-
   /*
    * if given group is the selected group, deselect it
    * else, select the given group
    */
+  $scope.shownGroup = 'video_1';
   $scope.toggleGroup = function(group) {
     if ($scope.isGroupShown(group)) {
       $scope.shownGroup = null;
