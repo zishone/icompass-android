8685ff5e14202b80ef8f02d97737f9338de73564
superzish <zjulbertg@gmail.com>
2017-03-22 08:48:50 +0800

    pain detect result patterned to esas result

--- www/js/controllers/takePainDetect.controller.js
+++ www/js/controllers/takePainDetect.controller.js
@@ -3,115 +3,122 @@ angular.module('iComPAsS.controllers')
 .controller('TakePainDetectCtrl', function($scope, $state, UsersService, PainDetectService){
   $scope.pain_detect_enabled = 1;
 
-  $scope.pain_detect_result = {
-    sliders: {
-      slider_1: 0,
-      slider_2: 0,
-      slider_3: 0
-    },
-    radio: {
-      radio_1: ''
-    },
-    questions: {
-      question_1: undefined,
-      question_2: undefined,
-      question_3: undefined,
-      question_4: undefined,
-      question_5: undefined,
-      question_6: undefined,
-      question_7: undefined
-    },
-    diagrams: {
-      anterior: {
-        "anterior_head_r": 0,
-        "anterior_head_l": 0,
-        "face_r": 0,
-        "face_l": 0,
-        "neck_r": 0,
-        "neck_midline": 0,
-        "neck_l": 0,
-        "anterior_shoulder_r": 0,
-        "upper_chest_r": 0,
-        "upper_chest_l": 0,
-        "anterior_shoulder_l": 0,
-        "anterior_upperarm_r": 0,
-        "lower_chest_r": 0,
-        "lower_chest_l": 0,
-        "anterior_upperarm_l": 0,
-        "antecubital_r": 0,
-        "hypochondriac_r": 0,
-        "epigastric": 0,
-        "hypochondriac_l": 0,
-        "antecubital_l": 0,
-        "umbilical": 0,
-        "lumbar_r": 0,
-        "anterior_forearm_r": 0,
-        "wrist_r": 0,
-        "hand_r": 0,
-        "anterior_thigh_r": 0,
-        "hip_r": 0,
-        "inguinal_r": 0,
-        "anterior_perineum": 0,
-        "inguinal_l": 0,
-        "hypogastric": 0,
-        "iliac_r": 0,
-        "iliac_l": 0,
-        "lumbar_l": 0,
-        "hip_l": 0,
-        "anterior_forearm_l": 0,
-        "anterior_thigh_l": 0,
-        "wrist_l": 0,
-        "hand_l": 0,
-        "knee_l": 0,
-        "knee_r": 0,
-        "anterior_ankle_r": 0,
-        "anterior_ankle_l": 0,
-        "anterior_leg_r": 0,
-        "foot_r": 0,
-        "foot_l": 0,
-        "anterior_leg_l": 0
+  var initializePainDetectResult = function() {
+    $scope.pain_detect_result = {
+      sliders: {
+        slider_1: 0,
+        slider_2: 0,
+        slider_3: 0
       },
-      posterior: {
-        "posterior_head_l": 0,
-        "posterior_head_r": 0,
-        "nape_l": 0,
-        "nape_midline": 0,
-        "nape_r": 0,
-        "posterior_shoulder_l": 0,
-        "upper_thorax_midline": 0,
-        "posterior_shoulder_r": 0,
-        "posterior_upperarm_r": 0,
-        "lower_thorax_r": 0,
-        "lower_thorax_midline": 0,
-        "lower_thorax_l": 0,
-        "posterior_upperarm_l": 0,
-        "posterior_forearm_l": 0,
-        "elbow_l": 0,
-        "posterior_hand_l": 0,
-        "buttock_l": 0,
-        "posterior_perineum": 0,
-        "buttock_r": 0,
-        "flank_r": 0,
-        "lower_back_midline": 0,
-        "flank_l": 0,
-        "elbow_r": 0,
-        "posterior_forearm_r": 0,
-        "posterior_hand_r": 0,
-        "posterior_thigh_r": 0,
-        "posterior_thigh_l": 0,
-        "posterior_leg_r": 0,
-        "posterior_leg_l": 0,
-        "popliteal_r": 0,
-        "popliteal_l": 0,
-        "posterior_ankle_l": 0,
-        "posterior_ankle_r": 0,
-        "foot_sole_l": 0,
-        "foot_sole_r": 0
-      }
-    }
+      radio: {
+        radio_1: ''
+      },
+      questions: {
+        question_1: undefined,
+        question_2: undefined,
+        question_3: undefined,
+        question_4: undefined,
+        question_5: undefined,
+        question_6: undefined,
+        question_7: undefined
+      },
+      diagrams: [
+        {
+          anterior: {
+            "anterior_head_r": 0,
+            "anterior_head_l": 0,
+            "face_r": 0,
+            "face_l": 0,
+            "neck_r": 0,
+            "neck_midline": 0,
+            "neck_l": 0,
+            "anterior_shoulder_r": 0,
+            "upper_chest_r": 0,
+            "upper_chest_l": 0,
+            "anterior_shoulder_l": 0,
+            "anterior_upperarm_r": 0,
+            "lower_chest_r": 0,
+            "lower_chest_l": 0,
+            "anterior_upperarm_l": 0,
+            "antecubital_r": 0,
+            "hypochondriac_r": 0,
+            "epigastric": 0,
+            "hypochondriac_l": 0,
+            "antecubital_l": 0,
+            "umbilical": 0,
+            "lumbar_r": 0,
+            "anterior_forearm_r": 0,
+            "wrist_r": 0,
+            "hand_r": 0,
+            "anterior_thigh_r": 0,
+            "hip_r": 0,
+            "inguinal_r": 0,
+            "anterior_perineum": 0,
+            "inguinal_l": 0,
+            "hypogastric": 0,
+            "iliac_r": 0,
+            "iliac_l": 0,
+            "lumbar_l": 0,
+            "hip_l": 0,
+            "anterior_forearm_l": 0,
+            "anterior_thigh_l": 0,
+            "wrist_l": 0,
+            "hand_l": 0,
+            "knee_l": 0,
+            "knee_r": 0,
+            "anterior_ankle_r": 0,
+            "anterior_ankle_l": 0,
+            "anterior_leg_r": 0,
+            "foot_r": 0,
+            "foot_l": 0,
+            "anterior_leg_l": 0
+          }
+        },
+        {
+          posterior: {
+            "posterior_head_l": 0,
+            "posterior_head_r": 0,
+            "nape_l": 0,
+            "nape_midline": 0,
+            "nape_r": 0,
+            "posterior_shoulder_l": 0,
+            "upper_thorax_midline": 0,
+            "posterior_shoulder_r": 0,
+            "posterior_upperarm_r": 0,
+            "lower_thorax_r": 0,
+            "lower_thorax_midline": 0,
+            "lower_thorax_l": 0,
+            "posterior_upperarm_l": 0,
+            "posterior_forearm_l": 0,
+            "elbow_l": 0,
+            "posterior_hand_l": 0,
+            "buttock_l": 0,
+            "posterior_perineum": 0,
+            "buttock_r": 0,
+            "flank_r": 0,
+            "lower_back_midline": 0,
+            "flank_l": 0,
+            "elbow_r": 0,
+            "posterior_forearm_r": 0,
+            "posterior_hand_r": 0,
+            "posterior_thigh_r": 0,
+            "posterior_thigh_l": 0,
+            "posterior_leg_r": 0,
+            "posterior_leg_l": 0,
+            "popliteal_r": 0,
+            "popliteal_l": 0,
+            "posterior_ankle_l": 0,
+            "posterior_ankle_r": 0,
+            "foot_sole_l": 0,
+            "foot_sole_r": 0
+          }
+        }
+      ]
+    };
   };
+  initializePainDetectResult();
 
-  
+
   $scope.translations = PainDetectService.get_pain_detect_translations();
 
   $scope.progress = {
@@ -195,11 +202,11 @@ angular.module('iComPAsS.controllers')
   ];
 
   $scope.anteriorColorSwitch = function(body_part_id){
-    $scope.pain_detect_result.diagrams.anterior[body_part_id] += 1;
-    $scope.pain_detect_result.diagrams.anterior[body_part_id] %= 3;
+    $scope.pain_detect_result.diagrams[0].anterior[body_part_id] += 1;
+    $scope.pain_detect_result.diagrams[0].anterior[body_part_id] %= 3;
 
     var num;
-    switch ($scope.pain_detect_result.diagrams.anterior[body_part_id]) {
+    switch ($scope.pain_detect_result.diagrams[0].anterior[body_part_id]) {
       case 0:
         num = 0;
         break;
@@ -217,11 +224,11 @@ angular.module('iComPAsS.controllers')
   };
 
   $scope.posteriorColorSwitch = function(body_part_id){
-    $scope.pain_detect_result.diagrams.posterior[body_part_id] += 1;
-    $scope.pain_detect_result.diagrams.posterior[body_part_id] %= 3;
+    $scope.pain_detect_result.diagrams[1].posterior[body_part_id] += 1;
+    $scope.pain_detect_result.diagrams[1].posterior[body_part_id] %= 3;
 
     var num;
-    switch ($scope.pain_detect_result.diagrams.posterior[body_part_id]) {
+    switch ($scope.pain_detect_result.diagrams[1].posterior[body_part_id]) {
       case 0:
         num = 0;
         break;
@@ -246,25 +253,7 @@ angular.module('iComPAsS.controllers')
 
       $scope.alertPopup('Success!', 'Have a great day!');
 
-      $scope.pain_detect_result = {
-        sliders: {
-          slider_1: 0,
-          slider_2: 0,
-          slider_3: 0
-        },
-        radio: {
-          radio_1: ''
-        },
-        questions: {
-          question_1: undefined,
-          question_2: undefined,
-          question_3: undefined,
-          question_4: undefined,
-          question_5: undefined,
-          question_6: undefined,
-          question_7: undefined
-        }
-      };
+      initializePainDetectResult();
 
       $scope.goHome();
     }, function(err) {
